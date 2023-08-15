cbf7ea6307641d91249c075a277ff302e4c55e71
SardorDev03 <142331681+SardorDev03@users.noreply.github.com>
2023-08-15 20:51:24 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Faylni qo'llash uchun joylashuv va nomlash
 const storage = multer.diskStorage({
